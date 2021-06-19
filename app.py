af98d23a20fd49d6d12f06342b435af69f81d4cd
Dog <48427669+hj231496@users.noreply.github.com>
2021-06-19 22:42:15 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def callback():
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
+    message = text=event.message.text
     if re.match('你會做什麼',message):
         line_bot_api.reply_message(event.reply,TextSendMessage('關你屁事'))
     else:
