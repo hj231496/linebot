d34ce81749b60292e519e61f8e12dbc1f5e04c50
Dog <48427669+hj231496@users.noreply.github.com>
2021-06-22 21:28:37 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -42,26 +42,25 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
      message = text=event.message.text
-     if re.match('告訴我秘密',message):
+     if re.match('天氣',message):
          buttons_template_message = TemplateSendMessage(
-         alt_text='這個看不到',
+         alt_text='天氣選單', #按鈕樣板介紹
          template=ButtonsTemplate(
              thumbnail_image_url='https://i.imgur.com/GGV9AQv.jpeg',
-             title='行銷搬進大程式',
-             text='選單功能－TemplateSendMessage',
+             title='你住在哪裡',
+             text='你他媽說喔',
              actions=[
-                 PostbackAction(
-                     label='偷偷傳資料',
-                     display_text='檯面上',
-                     data='action=檯面下'
+                 MessageAction(
+                     label='北部',
+                     text='北部'
                  ),
                  MessageAction(
-                     label='光明正大傳資料',
-                     text='我就是資料'
+                     label='中部',
+                     text='中部'
                  ),
-                 URIAction(
-                     label='行銷搬進大程式',
-                     uri='https://marketingliveincode.com/'
+                 MessageAction(
+                     label='南部',
+                     text='南部'
                  )
              ]
          )
