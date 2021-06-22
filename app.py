68ab9f98c3fe5f49336d7d550c57ac3e112bf5ee
Dog <48427669+hj231496@users.noreply.github.com>
2021-06-22 21:22:39 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Jun  2 21:16:35 2021
 
-@author: Ivan
-版權屬於「行銷搬進大程式」所有，若有疑問，可聯絡ivanyang0606@gmail.com
-
-Line Bot聊天機器人
-第一章 Line Bot申請與串接
-Line Bot機器人串接與測試
-"""
 #載入LineBot所需要的套件
 from flask import Flask, request, abort
 import re
@@ -28,10 +19,6 @@ line_bot_api = LineBotApi('1vmKFYz8QQclbvt3pdKXcjU40jdw21gmARxiLP0N6bB0dbM47tbAr
 handler = WebhookHandler('e2c72584113eafe24c4da5eadaa195d6')
 
 line_bot_api.push_message('U2d6e82f55b58b4877296ca5bcdb33856', TextSendMessage(text='你可以開始了'))
-for i in [1,2,3,4,5]:
-    line_bot_api.push_message('U2d6e82f55b58b4877296ca5bcdb33856', 
-                              TextSendMessage(text='我們來倒數：'+str(i)))
-    time.sleep(1)
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -54,17 +41,34 @@ def callback():
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = text=event.message.text
-
-    if re.match('告訴我秘密',message):
-        # 貼圖查詢：https://developers.line.biz/en/docs/messaging-api/sticker-list/#specify-sticker-in-message-object
-        sticker_message = StickerSendMessage(
-            package_id='1',
-            sticker_id='1'
-        )
-        line_bot_api.reply_message(event.reply_token, sticker_message)
-    else:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
+     message = text=event.message.text
+     if re.match('告訴我秘密',message):
+         buttons_template_message = TemplateSendMessage(
+         alt_text='這個看不到',
+         template=ButtonsTemplate(
+             thumbnail_image_url='https://i.imgur.com/GGV9AQv.jpeg',
+             title='行銷搬進大程式',
+             text='選單功能－TemplateSendMessage',
+             actions=[
+                 PostbackAction(
+                     label='偷偷傳資料',
+                     display_text='檯面上',
+                     data='action=檯面下'
+                 ),
+                 MessageAction(
+                     label='光明正大傳資料',
+                     text='我就是資料'
+                 ),
+                 URIAction(
+                     label='行銷搬進大程式',
+                     uri='https://marketingliveincode.com/'
+                 )
+             ]
+         )
+     )
+         line_bot_api.reply_message(event.reply_token, buttons_template_message)
+     else:
+         line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 
 #主程式
 import os
