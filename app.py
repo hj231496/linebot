894708bdee1f10508b3db542de6362edd292a4a8
Dog <48427669+hj231496@users.noreply.github.com>
2021-06-23 20:48:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import *
 import time
-#from weather import catch
+from weather import catch
 app = Flask(__name__)
 
 # 必須放上自己的Channel Access Token
@@ -51,8 +51,8 @@ def handle_message(event):
      )
          line_bot_api.reply_message(event.reply_token, buttons_template_message)
      
-     #if re.match('北部',message):
-         #line_bot_api.reply_message(event.reply_token, TextSendMessage(catch(message)))
+     if re.match('北部',message):
+         line_bot_api.reply_message(event.reply_token, TextSendMessage(catch(message)))
 
          
 #主程式
